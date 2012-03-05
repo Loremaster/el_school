b293a699350bd2b95c1594bbddf4517483a7478e
L Serj <gelvior@gmail.com>
2012-03-05 15:46:26 +0400

    Added some test for session controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,12 +1,51 @@
 require 'spec_helper'
 
 describe SessionsController do
-
+  render_views
+  
+  
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
       response.should be_success
-    end
+    end        
   end
 
+  describe "POST 'create'" do
+    describe "invalid signin" do
+      before(:each) do
+        @attr = { 
+                  :user_login => "login",
+                  :password => "invalid"
+                 }
+      end
+      
+      it "should have a flash.now message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+
+      it "should re-render the new page" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "with valid email and password" do
+      before(:each) do
+         @attr_user = { 
+                        :user_login => "Admin",
+                        :user_role => "admin",
+                        :password => "password"
+                      }
+         @user = User.create!( @attr_user )
+      end
+      
+      it "should sign the user in" do
+        post :create, :session => @attr_user
+        controller.current_user.should == @user
+        controller.should be_signed_in
+      end
+    end    
+  end  
 end
