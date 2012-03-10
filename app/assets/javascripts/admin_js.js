64a4912c76926cbed2c0178819c04b6b58378ea0
L Serj <gelvior@gmail.com>
2012-03-10 18:21:53 +0400

    Add new condition in js function of generating pass

--- app/assets/javascripts/admin_js.js
+++ app/assets/javascripts/admin_js.js
@@ -23,11 +23,18 @@ function generate_random_string( string_length ) {
 	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
 	var randomstring = '';
 
-	for (var i=0; i<string_length; i++) {
-		var rnum = Math.floor(Math.random() * chars.length);
-		randomstring += chars.substring(rnum, rnum+1);
-	}
+	if (string_length > 0) 
+	{
+		for (var i=0; i<string_length; i++) {
+			var rnum = Math.floor(Math.random() * chars.length);
+			randomstring += chars.substring(rnum, rnum+1);
+		}
 
-	return randomstring;
+		return randomstring;
+	} 
+	else
+	{
+		return null;
+	};
 }
 
