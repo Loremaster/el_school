654e67a17770532cfb1776cfb624c707f15387c6
L Serj <gelvior@gmail.com>
2012-03-13 14:20:19 +0400

    Wrong page now can be accesses only by sign in users.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  
+  before_filter  :authenticate, :only => :wrong_page                           #TODO write test for that
+
   #This page appears if signed in user can't access some page
   def wrong_page
   end
