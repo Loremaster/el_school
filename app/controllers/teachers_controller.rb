dc3060d9e97bcdf94b748df94090562c61f92be6
L Serj <gelvior@gmail.com>
2012-03-30 21:28:11 +0400

    Interface for subjects of teacher revised.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class TeachersController < ApplicationController
   before_filter :authenticate_school_heads, :only => [ :index, :edit, :update ]
   
@@ -8,17 +9,16 @@ class TeachersController < ApplicationController
   
   def edit
     @teacher = Teacher.find( params[:id] )
-    flash[:notice] = "#{@teacher.inspect}"
   end
   
   def update
     @teacher = Teacher.find( params[:id] )
     if @teacher.update_attributes(params[:teacher])
-      # redirect_to teachers_path
-      flash[:success] = "Successfully updated subjects for teacher!"
+      redirect_to teachers_path
+      flash[:success] = "Предметы учителя успешно обновлены!"
     else
-      # redirect_to edit_teacher_path
-      flash[:error] = "Subjects for teacher was not updated!"
+      redirect_to edit_teacher_path
+      flash[:error] = "Не удалось обновить предметы для учителя! Пожалуйста, попробуйте еще раз!"
     end
   end
 end
