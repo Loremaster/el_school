6fc982cf0a10a13d137287867d93d5c8cd13e0a0
L Serj <gelvior@gmail.com>
2012-04-11 21:00:45 +0400

    New tests for pupil model.

--- spec/models/pupil_spec.rb
+++ spec/models/pupil_spec.rb
@@ -45,5 +45,31 @@ describe Pupil do
         @user.create_pupil( @attr_pupil ).should be_valid  
       end.should change( Pupil, :count ).by( 1 )
     end
+    
+    it "should reject to create pupil via user with wrong attributes" do
+      expect do
+        @user.create_pupil( @attr_pupil.merge(:pupil_sex => '11') ).should_not be_valid  
+      end.should_not change( Pupil, :count ).by( 1 )
+    end
+  end
+  
+  describe "User-Pupil association" do
+    before(:each) do
+      @pupil = @user.create_pupil( @attr_pupil )
+    end
+    
+    it "should have a user attribute" do
+      @pupil.should respond_to( :user )
+    end    
+
+    it "should have the right associated user" do
+      @pupil.user_id.should == @user.id
+      @pupil.user.should == @user
+    end
+
+    it "should have the right associated school class" do
+      @pupil.school_class_id.should == @school_class.id
+      @pupil.school_class.should == @school_class
+    end
   end
 end
