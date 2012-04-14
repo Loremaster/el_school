8159c219fdc11d54fafbc67ee78599ca4556471c
L Serj <gelvior@gmail.com>
2012-04-14 17:51:20 +0400

    School head can edit pupil. Tests for put method added.

--- app/controllers/pupils_controller.rb
+++ app/controllers/pupils_controller.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 class PupilsController < ApplicationController
-  before_filter :authenticate_school_heads, :only => [ :index, :new, :create ]
+  before_filter :authenticate_school_heads, :only => [ :index, :new, :create, :edit, :update ]
   
   def index  
     @pupil_exist = Pupil.first ? true : false 
@@ -39,13 +39,30 @@ class PupilsController < ApplicationController
     pupil = Pupil.new( params[:pupil] )
     pupil.user.user_role = "pupil"
     
-    if pupil.save 
+    if pupil.save
+      redirect_to pupils_path 
       flash[:success] = "Ученик успешно создан!"
-      redirect_to pupils_path
     else
+      redirect_to new_pupil_path( params ) 
+      flash[:error] = pupil.errors.full_messages.to_sentence :last_word_connector => ", ",        
+                                                             :two_words_connector => ", "                                                             
+    end
+  end
+  
+  def edit
+    @pupil = Pupil.find( params[:id] )    
+  end
+  
+  def update    
+    pupil = Pupil.find( params[:id] )
+    
+    if pupil.update_attributes( params[:pupil] )
+      redirect_to pupils_path
+      flash[:success] = "Ученик успешно обновлен!"
+    else
+      redirect_to edit_pupil_path
       flash[:error] = pupil.errors.full_messages.to_sentence :last_word_connector => ", ",        
                                                              :two_words_connector => ", "
-      redirect_to new_pupil_path( params )                                                        
     end
   end
 end
--- spec/controllers/pupils_controller_spec.rb
+++ spec/controllers/pupils_controller_spec.rb
@@ -14,6 +14,7 @@ describe PupilsController do
     @sh.save!
     
     @user = Factory( :user )
+    @pupil = Factory( :pupil )
   end
   
   describe "GET 'index'" do
@@ -80,6 +81,86 @@ describe PupilsController do
         get :new
         response.should be_success
         flash[:error].should be_nil
+      end
+    end
+  end
+
+  describe "GET 'edit'" do
+    describe "for non-signed users" do
+      it "should deny access to edit pupil" do
+        get :edit, :id => @pupil
+        response.should redirect_to( signin_path )
+        flash[:notice].should =~ /войдите в систему как завуч/i
+      end
+    end
+    
+    describe "for signed-in admins" do
+      before(:each) do
+        test_sign_in( @adm )
+      end
+      
+      it "should deny access to edit pupil" do
+        get :edit, :id => @pupil
+        response.should redirect_to( pages_wrong_page_path )
+        flash[:error].should =~ /вы не можете увидеть эту страницу/i
+      end
+    end
+    
+    describe "for signed-in school-heads" do
+      before(:each) do
+        test_sign_in( @sh )
+      end
+      
+      it "should get page to edit pupil" do
+        get :edit, :id => @pupil
+        response.should be_success
+        flash[:error].should be_nil
+      end
+    end
+  end
+
+  describe "PUT 'update'" do
+    describe "for non-signed users" do
+      it "should deny access to update pupil" do
+        put :update, :id => @pupil, 
+                     :pupil => @pupil.attributes.merge(:pupil_last_name => "some") 
+        response.should redirect_to( signin_path )
+        flash[:notice].should =~ /войдите в систему как завуч/i             
+      end
+    end
+    
+    describe "for signed-in admins" do
+      before(:each) do
+        test_sign_in( @adm )
+      end
+      
+      it "should deny access to edit pupil" do
+        put :update, :id => @pupil, 
+                     :pupil => @pupil.attributes.merge(:pupil_last_name => "some")
+        response.should redirect_to( pages_wrong_page_path )
+        flash[:error].should =~ /вы не можете увидеть эту страницу/i
+      end
+    end
+    
+    describe "for signed-in school-heads" do
+      before(:each) do
+        test_sign_in( @sh )
+      end
+      
+      it "should reject to update pupil if params are not correct" do
+        last_name = "  "
+        put :update, :id => @pupil, 
+                     :pupil => @pupil.attributes.merge(:pupil_last_name => last_name)
+        @pupil.reload
+        @pupil.pupil_last_name.should_not == last_name
+      end
+      
+      it "should update pupil if params are correct" do
+        last_name = "last"
+        put :update, :id => @pupil, 
+                     :pupil => @pupil.attributes.merge(:pupil_last_name => last_name)
+        @pupil.reload
+        @pupil.pupil_last_name.should == last_name
       end
     end
   end
