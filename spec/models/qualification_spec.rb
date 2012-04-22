a6e30d6c238c52db2731bb29ff5d1b8301c0581a
L Serj <gelvior@gmail.com>
2012-04-22 20:18:25 +0400

    Tests for qualification model.

--- spec/models/qualification_spec.rb
+++ spec/models/qualification_spec.rb
@@ -9,8 +9,44 @@
 #  updated_at :datetime        not null
 #
 
-# require 'spec_helper'
-# 
-# describe Qualification do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'spec_helper'
+
+describe Qualification do
+  before(:each) do
+    @subject = FactoryGirl.create( :subject )
+    @teacher = FactoryGirl.create( :teacher )
+  end
+  
+  describe "Qualification creation" do
+    it "should be success" do
+      expect do
+        Qualification.create( :subject_id => @subject.id, :teacher_id => @teacher.id )
+      end.should change( Qualification, :count ).by( 1 ) 
+    end 
+  end
+  
+  describe "Qualification association" do
+    before(:each) do
+      @qualification = Qualification.create( :subject_id => @subject.id, 
+                                             :teacher_id => @teacher.id )
+    end
+    
+    it "should have a teacher attribute" do
+      @qualification.should respond_to(:teacher)
+    end
+    
+    it "should have a subject attribute" do
+      @qualification.should respond_to(:subject)
+    end
+    
+    it "should have the right associated teacher" do
+      @qualification.teacher_id.should == @teacher.id  
+      @qualification.teacher.should == @teacher        
+    end
+    
+    it "should have the right associated subject" do
+      @qualification.subject_id.should == @subject.id  
+      @qualification.subject.should == @subject        
+    end                                    
+  end
+end
