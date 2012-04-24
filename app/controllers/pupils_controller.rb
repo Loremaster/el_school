f0ef5ce024cb82af206ad2709609d0e89d5bcd5b
L Serj <gelvior@gmail.com>
2012-04-24 17:13:07 +0400

    Pupil refactored.

--- app/controllers/pupils_controller.rb
+++ app/controllers/pupils_controller.rb
@@ -22,16 +22,16 @@ class PupilsController < ApplicationController
     @pupil = Pupil.new; @user = User.new; @pupil_phone = PupilPhone.new     
   end
   
-  def create
-    case params[:pupil][:pupil_sex]
-      when 'm' then @pupil_sex_man, @pupil_sex_woman = true, false
-      when 'w' then @pupil_sex_man, @pupil_sex_woman = false, true
-    end
-    
+  def create   
     @login = params[:pupil][:user_attributes][:user_login]
     @password = params[:pupil][:user_attributes][:password]    
     @pupil = Pupil.new( params[:pupil] )
     @pupil.user.user_role = "pupil"
+        
+    case params[:pupil][:pupil_sex]
+      when 'm' then @pupil_sex_man, @pupil_sex_woman = true, false
+      when 'w' then @pupil_sex_man, @pupil_sex_woman = false, true
+    end    
         
     if @pupil.save
       redirect_to pupils_path 
@@ -56,7 +56,7 @@ class PupilsController < ApplicationController
   def update    
     @pupil = Pupil.find( params[:id] )
     
-    case @pupil.pupil_sex
+    case params[:pupil][:pupil_sex]
       when 'm' then @pupil_sex_man, @pupil_sex_woman = true, false
       when 'w' then @pupil_sex_man, @pupil_sex_woman = false, true
     end
--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,30 +10,26 @@ class SchoolClassesController < ApplicationController
   end
   
   def new
-    @everpresent_field_placeholder, @class_code = "Обязательное поле", ""
-    @creation_class_date = ""
+    @everpresent_field_placeholder = "Обязательное поле"
     @leaders = collect_teachers_leaders 
     @choosen_teacher = @leaders.first.last unless @leaders.empty?                         # First - array, then last element in array. So we get first teacher.    
     @class = SchoolClass.new 
-    @pupils = Pupil.order('pupil_last_name, pupil_first_name, pupil_middle_name')
-    
-    if params.has_key?( :school_class )
-      @class_code = params[:school_class][:class_code]
-      @creation_class_date = params[:school_class][:date_of_class_creation]
-      @choosen_teacher = params[:school_class][:teacher_leader_id]
-    end  
+    @pupils = Pupil.order('pupil_last_name, pupil_first_name, pupil_middle_name') 
   end
   
   def create
-    school_class = SchoolClass.new( params[:school_class] )
+    @everpresent_field_placeholder = "Обязательное поле"
+    @class = SchoolClass.new( params[:school_class] )
+    @leaders = collect_teachers_leaders
+    @choosen_teacher = @class.teacher_leader.id unless @class.nil?
     
-    if school_class.save
+    if @class.save
       redirect_to school_classes_path
       flash[:success] = "Класс успешно создан!"
     else
-      redirect_to new_school_class_path( params )
-      flash[:error] = school_class.errors.full_messages.to_sentence :last_word_connector => ", ",        
-                                                                    :two_words_connector => ", "
+      flash[:error] = @class.errors.full_messages.to_sentence :last_word_connector => ", ",        
+                                                              :two_words_connector => ", "
+      render 'new'
     end    
   end
   
@@ -47,15 +43,18 @@ class SchoolClassesController < ApplicationController
   end
   
   def update
-    school_class = SchoolClass.find( params[:id] )
+    @class = SchoolClass.find( params[:id] )
+    @leaders = collect_teachers_leaders  
+    @choosen_teacher = params[:school_class][:teacher_leader_id] 
+    @everpresent_field_placeholder = "Обязательное поле"
       
-    if school_class.update_attributes( params[:school_class] )      
+    if @class.update_attributes( params[:school_class] )      
       redirect_to school_classes_path
       flash[:success] = "Класс успешно обновлен!"
     else
-      redirect_to edit_school_class_path
-      flash[:error] = school_class.errors.full_messages.to_sentence :last_word_connector => ", ",        
-                                                                    :two_words_connector => ", "
+      flash[:error] = @class.errors.full_messages.to_sentence :last_word_connector => ", ",        
+                                                              :two_words_connector => ", "
+      render "edit"
     end  
   end
   
