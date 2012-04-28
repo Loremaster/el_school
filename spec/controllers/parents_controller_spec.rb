fae84dbb053c887f6082991e3f09192a57b68221
L Serj <gelvior@gmail.com>
2012-04-28 17:06:01 +0400

    Tests for Parent controller.

--- spec/controllers/parents_controller_spec.rb
+++ spec/controllers/parents_controller_spec.rb
@@ -86,6 +86,86 @@ describe ParentsController do
     end
   end
   
+  describe "GET 'edit'" do
+    describe "for non-signed users" do
+      it "should deny access" do
+        get :edit, :id => @parent
+        response.should redirect_to( signin_path )
+        flash[:notice].should =~ /войдите в систему как завуч/i
+      end
+    end
+    
+    describe "for signin-in admins" do
+      before(:each) do
+        test_sign_in( @adm )
+      end
+      
+      it "should deny access" do
+        get :edit, :id => @parent
+        response.should redirect_to( pages_wrong_page_path )
+        flash[:error].should =~ /вы не можете увидеть эту страницу/i
+      end
+    end
+    
+    describe "for sign-in school-heads" do
+      before(:each) do
+        test_sign_in( @sh )
+      end
+      
+      it "should allow to edit orders" do
+        get :edit, :id => @parent
+        response.should be_success
+        flash[:error].should be_nil
+      end
+    end
+  end
+  
+  describe "PUT 'update" do 
+    describe "for non-signed users" do
+      it "should deny access" do
+        put :update, :id => @parent, 
+                     :order => @parent.attributes.merge( :parent_last_name => "some" )
+        response.should redirect_to( signin_path )
+        flash[:notice].should =~ /войдите в систему как завуч/i
+      end
+    end
+    
+    describe "for signed-in admins" do
+      before(:each) do
+        test_sign_in( @adm )
+      end
+      
+      it "should deny access" do
+        put :update, :id => @parent, 
+                     :order => @parent.attributes.merge( :parent_last_name => "some" )
+        response.should redirect_to( pages_wrong_page_path )
+        flash[:error].should =~ /вы не можете увидеть эту страницу/i
+      end
+    end
+       
+    describe "for signed-in school-heads" do
+      before(:each) do
+        test_sign_in( @sh )
+      end
+      
+      it "should reject to update order with wrong params" do
+        text = "  "
+        put :update, :id => @parent, 
+                     :parent => @parent.attributes.merge( :parent_last_name => text )
+        @parent.reload
+        @parent.parent_last_name.should_not == text
+      end
+      
+      it "should update parent with correct params" do
+        text = "last"
+        put :update, :id => @parent, 
+                     :parent => @parent.attributes.merge( :parent_last_name => text )
+        @parent.reload
+        @parent.parent_last_name.should == text
+      end
+    end      
+  end
+  
   # describe "POST 'create'" do
   #     describe "for signed-in school-heads" do
   #       before(:each) do
