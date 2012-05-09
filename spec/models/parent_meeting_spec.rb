10f103a29cfa54ffa45e4e37d849a5d2c4903b38
L Serj <gelvior@gmail.com>
2012-05-09 19:47:51 +0400

    Tests added for ParentMeeting model.

--- spec/models/parent_meeting_spec.rb
+++ spec/models/parent_meeting_spec.rb
@@ -1,3 +1,4 @@
+# Created by 'bundle exec annotate --position before'
 # == Schema Information
 #
 # Table name: parent_meetings
@@ -12,5 +13,41 @@
 require 'spec_helper'
 
 describe ParentMeeting do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @parent = FactoryGirl.create( :parent )
+    @meeting = FactoryGirl.create( :meeting )
+  end
+  
+  describe "ParentMeeting creation" do
+    it "should be success" do
+      expect do
+        ParentMeeting.create(:parent_id => @parent.id, :meeting_id => @meeting.id  )
+      end.should change( ParentMeeting, :count ).by( 1 ) 
+    end
+  end
+  
+  describe "ParentMeeting association" do
+    before(:each) do
+      @parent_meeting = ParentMeeting.create( :parent_id => @parent.id, 
+                                              :meeting_id => @meeting.id )
+    end
+    
+    it "should have a meeting attribute" do
+      @parent_meeting.should respond_to( :meeting )
+    end
+    
+    it "should have a parent attribute" do
+      @parent_meeting.should respond_to( :parent )
+    end
+    
+    it "should have the right associated meeting" do
+      @parent_meeting.meeting_id.should == @meeting.id  
+      @parent_meeting.meeting.should == @meeting        
+    end
+    
+    it "should have the right associated parent" do
+      @parent_meeting.parent_id.should == @parent.id  
+      @parent_meeting.parent.should == @parent        
+    end
+  end
 end
