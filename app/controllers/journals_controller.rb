d8e46accb0bbf15381073743f9a922f76c0ceadf
L Serj <gelvior@gmail.com>
2012-05-11 22:54:16 +0400

    Choosing class button appears AFTER choosing subject which teacher teaches.

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -2,9 +2,9 @@ class JournalsController < ApplicationController
   before_filter :authenticate_teachers, :only => [ :index ]
 
   def index
+    @subject = []
     @teacher_subjects = current_user.teacher.subjects
-    subject = Subject.where( "subject_name = ?", params[:subject_name] )                  # This we get when user choose subject from toolbar.
+    @subject = Subject.where( "subject_name = ?", params[:subject_name] )                 # This we get when user choose subject from toolbar.
     @classes = SchoolClass.all
-    # flash.now[:notice] = "#{subject.inspect}"
   end
 end
