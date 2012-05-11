83298eb08443539b157d3c909996268190cec4c3
L Serj <gelvior@gmail.com>
2012-05-12 00:04:14 +0400

    Minor.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 # encoding: UTF-8
+# Created by 'bundle exec annotate --position before'
 # == Schema Information
 #
 # Table name: events
@@ -20,63 +21,63 @@
 class Event < ActiveRecord::Base
   belongs_to :school_class
   belongs_to :teacher
-  
+
   validates :school_class_id, :presence => { :message => "должен быть указан" }
-  
+
   validates :teacher_id, :presence => { :message => "должен быть указан" }
-  
+
   validates :event_place,
-              :presence   => { :message => "не может быть пустым" },            
+              :presence   => { :message => "не может быть пустым" },
               :length     => { :maximum => 200,
-                               :message => "должно содержать не более %{count} символов" 
+                               :message => "должно содержать не более %{count} символов"
                              }
-                             
-  validates :event_place_of_start,                                                   
-              :presence   => { :message => "не может быть пустым" },                 
-              :length     => { :maximum => 200,                                     
-                               :message => "должно содержать не более %{count} символов"  
+
+  validates :event_place_of_start,
+              :presence   => { :message => "не может быть пустым" },
+              :length     => { :maximum => 200,
+                               :message => "должно содержать не более %{count} символов"
                              }
-                          
+
   validates :event_begin_date,
-              :inclusion => { :in => ( Date.today - 1.year )..( Date.today + 1.year ),                
-                              :message => "должна находиться в пределах одного года от текущей даты"  
+              :inclusion => { :in => ( Date.today - 1.year )..( Date.today + 1.year ),
+                              :message => "должна находиться в пределах одного года от текущей даты"
                             }
 
-  validates :event_end_date,                                                                                                   
-              :inclusion => { :in => ( Date.today - 1.year )..( Date.today + 1.year ),                                          
-                              :message => "должна находиться в пределах одного года от текущей даты"                            
-                            }                                                                                                  
+  validates :event_end_date,
+              :inclusion => { :in => ( Date.today - 1.year )..( Date.today + 1.year ),
+                              :message => "должна находиться в пределах одного года от текущей даты"
+                            }
 
-  validates :event_begin_time, :presence => { :message => "не может быть пустым" }                                                                    
+  validates :event_begin_time, :presence => { :message => "не может быть пустым" }
 
-  validates :event_end_time, :presence => { :message => "не может быть пустым" }                                                                 
-                                         
+  validates :event_end_time, :presence => { :message => "не может быть пустым" }
+
   validates :event_cost,
               :presence => { :message => "не может быть пустой" },
               :numericality => { :message => "должна являться целым числом",
-                                 :only_integer => true }            
- 
+                                 :only_integer => true }
+
   # I use diffirents validations for one field because then it shows messages correctly.
-  validates :event_cost,  
+  validates :event_cost,
               :numericality => {  :greater_than_or_equal_to => 0, :message => "должна быть не меньше 0" }
-                               
-  validates :event_cost,                                                                                                      
-              :numericality => { :less_than => 100000, :message => "должна быть меньше 100000" }                                                                
-              
+
+  validates :event_cost,
+              :numericality => { :less_than => 100000, :message => "должна быть меньше 100000" }
+
   validate :start_must_be_before_or_eq_end_date
   validate :start_must_be_before_or_eq_end_time
-  
+
   def start_must_be_before_or_eq_end_date
     if not self.event_begin_date.nil? and not self.event_end_date.nil?
       errors.add(:event_begin_date, "должна быть меньше либо равной дате окончания") unless
         self.event_begin_date <= self.event_end_date
-    end   
-  end 
-  
+    end
+  end
+
   def start_must_be_before_or_eq_end_time
     if not self.event_begin_time.nil? and not self.event_end_time.nil?
       errors.add(:event_begin_time, "должно быть меньше либо равно дате окончания") unless
         self.event_begin_time <= self.event_end_time
-    end   
-  end           
+    end
+  end
 end
