895e729edd28fc63b31e479b54fe15dccda4ddcd
L Serj <gelvior@gmail.com>
2012-05-12 21:33:49 +0400

    Now teacher can create new date for lesson.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,16 @@ require 'date'
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-  
+
   #Check that date has format - dd.mm.yyyy.
   #Check that such date exists.
   def date_valid? ( date_in_str )
     regex = /\A\d{2}\.\d{2}\.\d{4}\z/                                                     # 2 digits.2 digits.4 digits
-    
+
     if date_in_str =~ regex
        date_to_parse = date_in_str.gsub('.', '/')                                         # Using / instead of . because such version can pe parsed
        begin
-          date = Date.strptime( date_to_parse, '%d/%m/%Y' ) 
+          date = Date.strptime( date_to_parse, '%d/%m/%Y' )
           Date.parse( date.to_s )
           return true
        rescue
@@ -21,29 +21,36 @@ class ApplicationController < ActionController::Base
        end
     else
        return false
-    end   
+    end
   end
-  
+
   def get_class_code( current_user )
     current_user.teacher_leader.school_class.class_code
   end
-  
+
   # Get class code for current class head.
   def get_class( current_user )
     current_user.teacher_leader.school_class
   end
-  
+
   # Get parent for class via class code.
   # => [] if there were no parents.
   def get_parents_for_class( class_code )
     parents = []                                                                          # Output.
     pupils = Pupil.select{|p| p.school_class.class_code == class_code }                   # Get pupils for class via class_code of class.
     pupils.each{ |p| parents << p.parents }                                               # Get parents for each pupil.
-    
+
     if parents.empty?
       parents
     else
       parents.flatten!.uniq!                                                              # Turn multy-dimension array into one dimension array (each parent for pupil got their array). And then keep only unique parents.
     end
+  end
+
+  # Day of week to russian word.
+  def translate_day_of_week( day )
+    days = { :Mon => "Понедельник", :Tue => "Вторник", :Wed => "Среда", :Thu => "Четверг",
+             :Fri => "Пятница" }
+    days[ day.to_sym ]
   end
 end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,62 @@
+# encoding: UTF-8
 class LessonsController < ApplicationController
   before_filter :authenticate_teachers, :only => [ :new, :create ]
 
   def new
     @teacher_subjects = current_user.teacher.subjects
+    subject = Subject.where( "subject_name = ?", params[:subject_name] ).first
+    school_class = SchoolClass.where( "class_code = ?", params[:class_code] )
+    timetables = timetables_for_teacher_with_subject( current_user.teacher, subject.subject_name )
+    @timetables_collection = timetables_for_select_list( timetables )
 
+    @lesson = Lesson.new
+    @everpresent_field_placeholder = "Обязательное поле"
+
+    flash[:notice] = "#{@timetables_collection}"
   end
 
   def create
+    @teacher_subjects = current_user.teacher.subjects
+    @everpresent_field_placeholder = "Обязательное поле"
+    subject = Subject.where( "subject_name = ?", params[:subject_name] ).first
+    timetables = timetables_for_teacher_with_subject( current_user.teacher, subject.subject_name )
+    @timetables_collection = timetables_for_select_list( timetables )
 
+    @lesson = Lesson.new( params[:lesson] )
+
+    if @lesson.update_attributes( params[:lesson] )
+      redirect_to journals_path( :class_code => params[:class_code],
+	                               :subject_name => params[:subject_name] )
+      flash[:success] = "Дата успешно создана!"
+    else
+      flash.now[:error] = @lesson.errors.full_messages
+                                        .to_sentence :last_word_connector => ", ",
+                                                     :two_words_connector => ", "
+      render 'new'
+    end
   end
+
+  private
+
+    # Get ALL timetables for teacher
+    # => Array
+    def timetables_for_teacher_with_subject( teacher, subject_name )
+      tt = []                                                                             # Output timetables
+      subject = teacher.subjects.where(:subject_name => subject_name).first               # Get subject for teacher.
+      subject_qualification = teacher.qualifications.where(:subject_id => subject.id).first # Qualification for subject.
+      curriculums = subject_qualification.curriculums                                     # Get all curriculums
+      curriculums.each { |c| tt << c.timetables  }                                        # Collecting all timetables.
+      tt.flatten!                                                                         # To 1 dimension array (because of many-to-one).
+    end
+
+    # Retutn all timetables for timetable.
+    # Input timetable should be array!
+    # => Array
+    def timetables_for_select_list( timetables )
+      raise TypeError, "Input timetables should be Array!" unless timetables.kind_of?(Array) # It is REALLY important to be array.
+
+      timetables.collect do |tt|
+        ["#{translate_day_of_week(tt.tt_day_of_week)} - #{tt.tt_number_of_lesson}й урок", tt.id]
+      end
+    end
 end
