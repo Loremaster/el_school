e5e36a2df0638b1953530c2f1af5d9d2472e24ed
L Serj <gelvior@gmail.com>
2012-05-12 12:43:27 +0400

    Button to add new date for class added.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,7 @@ class LessonsController < ApplicationController
   before_filter :authenticate_teachers, :only => [ :new, :create ]
 
   def new
+    @teacher_subjects = current_user.teacher.subjects
 
   end
 
