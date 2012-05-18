82e5098f25a50e930ca4ff1ad44d0ec7d105b5c1
L Serj <gelvior@gmail.com>
2012-05-19 00:11:20 +0400

    New factories added.

--- spec/factories.rb
+++ spec/factories.rb
@@ -25,7 +25,7 @@ FactoryGirl.define do
 
   factory :subject do |subject|
     subject.subject_name 'Math'
-  end 
+  end
 
   factory :teacher_leader do |tl|
     tl.user
@@ -36,7 +36,7 @@ FactoryGirl.define do
     c.date_of_class_creation "#{Date.today}"
     c.class_code '11v'
     c.teacher_leader
-  end   
+  end
 
   factory :pupil do |p|
     p.pupil_last_name 'Kopov'
@@ -52,23 +52,23 @@ FactoryGirl.define do
   end
 
   factory :pupil_phone do |pp|
-    pp.pupil_home_number   '123456'   
+    pp.pupil_home_number   '123456'
     pp.pupil_mobile_number '65454343'
     pp.pupil
   end
-  
+
   factory :teacher_phone do |tp|
     tp.teacher_home_number '13575432'
     tp.teacher_mobile_number '23456778'
   end
-  
+
   factory :teacher_education do |te|
     te.teacher_education_university 'MIT'
     te.teacher_education_year       "#{Date.today - 10.years}"
     te.teacher_education_graduation 'Programmer'
     te.teacher_education_speciality 'Math programmer'
   end
-  
+
   factory :order do |o|
     o.number_of_order '1234'
     o.date_of_order "#{Date.today}"
@@ -76,7 +76,7 @@ FactoryGirl.define do
     o.pupil
     o.school_class
   end
-  
+
   factory :parent do |p|
     p.parent_last_name   'Skobkin'
     p.parent_first_name  'Serj'
@@ -85,12 +85,12 @@ FactoryGirl.define do
     p.parent_sex         'm'
     p.user
   end
-  
+
   factory :parent_pupil do |pp|
     pp.parent
     pp.pupil
   end
-  
+
   factory :meeting do |m|
     m.meeting_theme 'Some theme!'
     m.meeting_date  "#{Date.today}"
@@ -98,17 +98,17 @@ FactoryGirl.define do
     m.meeting_room  '123'
     m.school_class
   end
-  
+
   factory :qualification do |q|
     q.teacher
     q.subject
   end
-  
+
   factory :curriculum do |c|
     c.qualification
     c.school_class
   end
-  
+
   factory :event do |e|
     e.school_class
     e.teacher
@@ -120,15 +120,22 @@ FactoryGirl.define do
     e.event_end_time  Time.now.strftime("%H:%M")
     e.event_cost  "500"
   end
-  
-  # factory :timetable do |t|
-  #   t.curriculum
-  #   t.school_class
-  #   t.tt_day_of_week 'Mon'
-  #   t.tt_number_of_lesson '1'
-  #   t.tt_room '123'
-  #   t.tt_type 'Primary lesson'
-  # end
+
+  factory :timetable do |t|
+    t.curriculum
+    t.school_class
+    t.tt_day_of_week 'Mon'
+    t.tt_number_of_lesson '1'
+    t.tt_room '123'
+    t.tt_type 'Primary lesson'
+  end
+
+  factory :lesson do |l|
+    l.timetable
+    l.lesson_date "#{Date.today.strftime("%d.%m.%Y")}"
+  end
+
+
 end
 
 
