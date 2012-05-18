93764d8265a24ed663403ed0638b47c48dc851c3
L Serj <gelvior@gmail.com>
2012-05-19 00:41:00 +0400

    New/create methods added.

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,12 @@
+# encoding: UTF-8
 class AttendancesController < ApplicationController
+  before_filter :authenticate_teachers, :only => [ :new, :create ]
+
+  def new
+
+  end
+
+  def create
+
+  end
 end
