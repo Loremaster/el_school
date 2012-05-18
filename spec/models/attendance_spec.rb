462cf62e847ef76bf6f147b066c453ab9798797a
L Serj <gelvior@gmail.com>
2012-05-19 00:17:09 +0400

    Tests for Attendance model added.

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -1,3 +1,4 @@
+# Created by 'bundle exec annotate --position before'
 # == Schema Information
 #
 # Table name: attendances
@@ -12,5 +13,40 @@
 require 'spec_helper'
 
 describe Attendance do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @pupil = FactoryGirl.create( :pupil )
+    @lesson = FactoryGirl.create( :lesson )
+  end
+
+  describe "Attendance creation" do
+    it "should be success" do
+      expect do
+        Attendance.create( :pupil_id => @pupil.id, :lesson_id => @lesson.id )
+      end.should change( Attendance, :count ).by( 1 )
+    end
+  end
+
+  describe "Attendance association" do
+    before(:each) do
+      @attendance = Attendance.create( :pupil_id => @pupil.id, :lesson_id => @lesson.id )
+    end
+
+    it "should have a pupil attribute" do
+      @attendance.should respond_to( :pupil )
+    end
+
+    it "should have a lesson attribute" do
+      @attendance.should respond_to( :lesson )
+    end
+
+    it "should have the right associated pupil" do
+      @attendance.pupil_id.should == @pupil.id
+      @attendance.pupil.should == @pupil
+    end
+
+    it "should have the right associated lesson" do
+      @attendance.lesson_id.should == @lesson.id
+      @attendance.lesson.should == @lesson
+    end
+  end
 end
