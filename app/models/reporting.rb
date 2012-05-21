c73bbaf411162eeda26d8c6fc90e6c170098fce6
L Serj <gelvior@gmail.com>
2012-05-21 22:18:02 +0400

    Validations with messages added.

--- app/models/reporting.rb
+++ app/models/reporting.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # == Schema Information
 #
 # Table name: reportings
@@ -12,4 +13,20 @@
 
 class Reporting < ActiveRecord::Base
   belongs_to :lesson
+
+  validates :lesson_id, :presence => { :message => "должен быть указан" }
+
+  # homework - домашняя работа.
+  # classwork - работа на уроке.
+  # labwork - лабораторная работа.
+  # checkpoint - контрольная работа.
+  validates :report_type,
+              :presence  => { :message => "должен быть указан" },
+              :inclusion => { :in => %w(homework classwork labwork checkpoint) }
+
+  validates :report_topic,
+              :length     => {
+                               :maximum => 200,
+                               :message => "должна содержать не более %{count} символов"
+                             }
 end
