f4bf8859fb34ee5c2c077e77ae3edd1704fcf048
L Serj <gelvior@gmail.com>
2012-05-21 22:38:33 +0400

    Tests for reporting validations.

--- spec/models/reporting_spec.rb
+++ spec/models/reporting_spec.rb
@@ -13,5 +13,74 @@
 require 'spec_helper'
 
 describe Reporting do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @lesson = FactoryGirl.create( :lesson )
+    @attr_rep = { :lesson_id => @lesson.id, :report_type => "homework", :report_topic => "" }
+  end
+
+  describe "Reporting creation" do
+    it "should be success" do
+      expect do
+        Reporting.create( @attr_rep )
+      end.should change( Reporting, :count ).by( 1 )
+    end
+
+    it "should not success with invalid params" do
+      expect do
+        Reporting.create( @attr_rep.merge( :lesson_id => nil, :report_type => "" ) )
+      end.should_not change( Reporting, :count )
+    end
+  end
+
+  describe "Attendance association" do
+    before(:each) do
+      @reporting = Reporting.create( @attr_rep )
+    end
+
+    it "should have a lesson attribute" do
+      @reporting.should respond_to( :lesson )
+    end
+
+    it "should have the right associated lesson" do
+      @reporting.lesson_id.should == @lesson.id
+      @reporting.lesson.should == @lesson
+    end
+  end
+
+  describe "Validations" do
+    describe "Rejection" do
+      it "should reject nil lesson id" do
+        wrong_attr = @attr_rep.merge( :lesson_id => nil )
+        Reporting.new( wrong_attr ).should_not be_valid
+      end
+
+      it "should reject invalid report type" do
+        invalid_type = %w(1 2 sd dfdf gfdsds dfsdfd1)
+        invalid_type.each do |i|
+          Reporting.new( @attr_rep.merge( :report_type => i ) ).should_not be_valid
+        end
+      end
+
+      it "should reject too long report topic" do
+        wrong_attr = @attr_rep.merge( :report_topic => 'a' * 201 )
+        Reporting.new( wrong_attr ).should_not be_valid
+      end
+    end
+
+    describe "Acceptance" do
+      it "should accept correct report type" do
+        valid_type = %w(homework classwork labwork checkpoint)
+        valid_type.each do |i|
+          Reporting.new( @attr_rep.merge( :report_type => i ) ).should be_valid
+        end
+      end
+
+      it "should accept report topic if length is correct" do
+        (1..200).each do |i|
+          correct_attr = @attr_rep.merge( :report_topic => 'a' * i  )
+          Reporting.new( correct_attr ).should be_valid
+        end
+      end
+    end
+  end
 end
