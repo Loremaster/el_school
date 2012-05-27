3fcbfa0a3e89eaccd1e6cf00026171b5f95b0c98
L Serj <gelvior@gmail.com>
2012-05-27 21:34:11 +0400

    Result model annotated.

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: results
+#
+#  id            :integer         not null, primary key
+#  pupil_id      :integer
+#  curriculum_id :integer
+#  quarter_1     :integer
+#  quarter_2     :integer
+#  quarter_3     :integer
+#  quarter_4     :integer
+#  year          :integer
+#  created_at    :datetime        not null
+#  updated_at    :datetime        not null
+#
+
 class Result < ActiveRecord::Base
 end
--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: results
+#
+#  id            :integer         not null, primary key
+#  pupil_id      :integer
+#  curriculum_id :integer
+#  quarter_1     :integer
+#  quarter_2     :integer
+#  quarter_3     :integer
+#  quarter_4     :integer
+#  year          :integer
+#  created_at    :datetime        not null
+#  updated_at    :datetime        not null
+#
+
 require 'spec_helper'
 
 describe Result do
