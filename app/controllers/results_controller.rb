f830e2e75b3c23f63ea640a552573fbc0931680a
L Serj <gelvior@gmail.com>
2012-05-27 21:23:42 +0400

    Index page (template) of Result controller created with some code.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,11 @@
 class ResultsController < ApplicationController
   before_filter :authenticate_teachers, :only => [ :index ]
-  
+
   def index
-    
+    @subject = []; @pupils = []
+    @classes = SchoolClass.all
+    @subject, @school_class = extract_class_code_and_subj_name( params, :subject_name, :class_code )
+    @pupils = get_pupils_for_class( @school_class )                                       # Pupils in the class.
+    @pupils_exist = @pupils.first ? true : false
   end
 end
