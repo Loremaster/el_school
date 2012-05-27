af67a7d964b28deecf72ca4db47f1ad515a38068
L Serj <gelvior@gmail.com>
2012-05-27 20:52:26 +0400

    Show created nominal when we visit edit page. Keep nominal after redirect in create/update.

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -10,6 +10,7 @@ class AttendancesController < ApplicationController
     @pupil = $pupil; @lesson = $lesson                                                    # Using global value to save object from params.
     @report_types = collect_report_types
     @reporting = @lesson.reporting; @nominals = collect_nominals
+    @choosen_nominal = nil
     @attendance = Attendance.new
     @estimation = Estimation.new
   end
@@ -20,6 +21,7 @@ class AttendancesController < ApplicationController
     @subject, school_class = extract_class_code_and_subj_name( params, :subject_name, :class_code )
     @pupil = $pupil; @lesson = $lesson
     @reporting = @lesson.reporting; @nominals = collect_nominals
+    @choosen_nominal = params[:estimation][:nominal]
     @attendance = Attendance.new( params[:attendance] )
     @estimation = Estimation.new( params[:estimation] )
 
@@ -53,6 +55,7 @@ class AttendancesController < ApplicationController
     @reporting = @lesson.reporting; @nominals = collect_nominals
     @attendance = Attendance.find( params[:id] )
     @estimation = estimation_of_pupil_from_lesson( @lesson, @pupil.id )
+    @choosen_nominal = @estimation.nominal
   end
 
   def update
@@ -63,6 +66,7 @@ class AttendancesController < ApplicationController
     @reporting = @lesson.reporting; @nominals = collect_nominals
     @attendance = Attendance.find( params[:id] )
     @estimation = estimation_of_pupil_from_lesson( @lesson, @pupil.id )
+    @choosen_nominal = params[:estimation][:nominal]
 
     temp_att = Attendance.new( params[:attendance] )                                      # Hack to test that new objects will valid, so we can
     temp_est = Estimation.new( params[:estimation] )                                      # update them and show all errors.
