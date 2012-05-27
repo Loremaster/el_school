5e816b00d793509954c5db20769ba27e88e917c7
L Serj <gelvior@gmail.com>
2012-05-28 01:33:11 +0400

    Results now can be created via teacher.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class ResultsController < ApplicationController
   before_filter :authenticate_teachers, :only => [ :index, :new, :create ]
 
@@ -11,12 +12,31 @@ class ResultsController < ApplicationController
 
   def new
     @subject, @school_class = extract_class_code_and_subj_name( params, :subject_name, :class_code )
-    $pupil = get_pupil_from_params( params ); @pupil = $pupil
+    $pupil = get_pupil_from_params( params ); @pupil = $pupil; @nominals = collect_nominals
+    @curriculum = curriculum_for_teacher_with_subject_and_class( current_user.teacher,
+                                                                 @subject.subject_name,
+                                                                 @school_class )
     @result = Result.new
   end
 
   def create
+    @subject, @school_class = extract_class_code_and_subj_name( params, :subject_name, :class_code )
+    @pupil = $pupil; @nominals = collect_nominals
+    @curriculum = curriculum_for_teacher_with_subject_and_class( current_user.teacher,
+                                                                 @subject.subject_name,
+                                                                 @school_class )
+    @result = Result.new( params[:result] )
 
+    if @result.save
+      redirect_to results_path( :class_code => params[:class_code],
+                                :subject_name => params[:subject_name] )
+      flash[:success] = "Итоги успешно созданы!"
+    else
+      flash.now[:error] = @result.errors.full_messages
+                                        .to_sentence :last_word_connector => ", ",
+                                                     :two_words_connector => ", "
+      render 'new'
+    end
   end
 
   private
