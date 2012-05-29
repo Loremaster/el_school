dc978c5c9d049ab4e52645cf8f5a2654b30b8c00
L Serj <gelvior@gmail.com>
2012-05-29 22:29:15 +0400

    Homework index page created. How we can choose there class.

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -1,4 +1,9 @@
 # encoding: UTF-8
 class HomeworksController < ApplicationController
   before_filter :authenticate_teachers, :only => [ :index ]
+
+  def index
+    @subject = []; @pupils = []; @classes = SchoolClass.all
+    @subject, @school_class = extract_class_code_and_subj_name( params, :subject_name, :class_code )
+  end
 end
