daaf53ccde061f818a74b0957ac9611b87a29786
L Serj <gelvior@gmail.com>
2012-05-29 21:06:37 +0400

    Tests for Update method of results controller added.

--- spec/controllers/results_controller_spec.rb
+++ spec/controllers/results_controller_spec.rb
@@ -25,7 +25,7 @@ describe ResultsController do
 
   describe "GET 'index'" do
     describe "for non-signed users" do
-      it "should deny access to show orders" do
+      it "should deny access to show results" do
         get :index, { :subject_name => @subject_name, :class_code => @class_code }
         response.should redirect_to( signin_path )
         flash[:notice].should =~ /войдите в систему как учитель/i
@@ -59,7 +59,7 @@ describe ResultsController do
 
   describe "GET 'new'" do
     describe "for non-signed users" do
-      it "should deny access to show orders" do
+      it "should deny access to show results" do
         get :new, { :subject_name => @subject_name, :class_code => @class_code,
                     :p_id => @pupil.id }
         response.should redirect_to( signin_path )
@@ -96,7 +96,7 @@ describe ResultsController do
 
   describe "GET 'edit'" do
     describe "for non-signed users" do
-      it "should deny access to show orders" do
+      it "should deny access to show results edit page" do
         get :edit, { :id => @result, :subject_name => @subject_name,
                      :class_code => @class_code }
         response.should redirect_to( signin_path )
@@ -127,6 +127,52 @@ describe ResultsController do
                      :class_code => @class_code }
         response.should be_success
         flash[:error].should be_nil
+      end
+    end
+  end
+
+  describe "PUT 'update" do
+    describe "for non-signed users" do
+      it "should deny access to update results" do
+        put :update, :id => @result,
+                     :result => @result.attributes.merge( :quarter_1 => 5 )
+        response.should redirect_to( signin_path )
+        flash[:notice].should =~ /войдите в систему как учитель/i
+      end
+    end
+
+    describe "for signed-in admins" do
+      before(:each) do
+        test_sign_in( @adm )
+      end
+
+      it "should deny access to update results" do
+        put :update, :id => @result,
+                     :result => @result.attributes.merge( :quarter_1 => 5 )
+        response.should redirect_to( pages_wrong_page_path )
+        flash[:error].should =~ /вы не можете увидеть эту страницу/i
+      end
+    end
+
+    describe "for signed-in teachers" do
+      before(:each) do
+        test_sign_in( @tch )
+      end
+
+      it "should update results with valid params" do
+        value = 5
+        put :update, :id => @result,
+                     :result => @result.attributes.merge( :quarter_1 => value )
+        @result.reload
+        @result.quarter_1.should == value
+      end
+
+      it "should not update results with invalid params" do
+        value = 0
+        put :update, :id => @result,
+                     :result => @result.attributes.merge( :quarter_1 => value )
+        @result.reload
+        @result.quarter_1.should_not == value
       end
     end
   end
