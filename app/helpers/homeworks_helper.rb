2dcba5c0fe926cc6d75dbfe33dc6f30f2e322a57
L Serj <gelvior@gmail.com>
2012-05-31 18:23:43 +0400

    homeworks_helper improved.

--- app/helpers/homeworks_helper.rb
+++ app/helpers/homeworks_helper.rb
@@ -1,8 +1,9 @@
 # encoding: UTF-8
 module HomeworksHelper
-  # => lesson_date - number_of_lesson
+  # => lesson_date - day - number_of_lesson
   def homework_lesson_info( homework )
     "#{homework.lesson.lesson_date.strftime("%d.%m.%Y")} - " +
+    "#{translate_day_of_week(homework.lesson.timetable.tt_day_of_week)} - " +
     "#{homework.lesson.timetable.tt_number_of_lesson}й урок"
   end
 end
