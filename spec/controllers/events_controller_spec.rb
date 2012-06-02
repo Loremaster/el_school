79af9b9e578a529916a4a215cb4019d348bbaa14
L Serj <gelvior@gmail.com>
2012-06-02 13:41:29 +0400

    index_for_parent for event controller test added.

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -3,27 +3,55 @@ require 'spec_helper'
 
 describe EventsController do
   render_views
-  
+
   before(:each) do
     @adm = FactoryGirl.create( :user )
     @adm.user_role = "admin"
     @adm.save!
-        
+
     @sh = FactoryGirl.create( :user )
     @sh.user_role = "school_head"
-    @sh.save!    
-        
+    @sh.save!
+
     # In event controller we get school class code for teacher_leader.
     # So here we do a trick. We create teacher leader via school class and make it's
-    # user's role class_head.    
-    school_class = FactoryGirl.create( :school_class )    
+    # user's role class_head.
+    school_class = FactoryGirl.create( :school_class )
     @ch = school_class.teacher_leader.user
     @ch.user_role = "class_head"
     @ch.save
-    
+
     @event = FactoryGirl.create( :event )
   end
-  
+
+  describe "GET 'index_for_parent'" do
+    before(:each) do
+      # We create user this way because it auto creates school class (controller need that.)
+      @pupil = FactoryGirl.create( :pupil )
+      @pupil.user.user_role = "pupil"
+      @pupil.user.save!
+
+      # Creating parent and link to his child - pupil.
+      pp = FactoryGirl.create( :parent_pupil, :pupil_id => @pupil.id )
+      pp.parent.user.user_role = "parent"
+      pp.parent.user.save!
+
+      @parent = pp.parent.user
+    end
+
+    describe "for signed-in parents" do
+      before(:each) do
+        test_sign_in( @parent )
+      end
+
+      it "should show" do
+        get :index_for_parent, { :p_id => @pupil.id }
+        response.should be_success
+        flash[:error].should be_nil
+      end
+    end
+  end
+
   describe "GET 'index_school_head'" do
     describe "for non-signed users" do
       it "should deny access to show events for school head" do
@@ -32,24 +60,24 @@ describe EventsController do
         flash[:notice].should =~ /войдите в систему как завуч/i
       end
     end
-    
+
     describe "for signed-in admins" do
       before(:each) do
         test_sign_in( @adm )
       end
-      
+
       it "should deny access to show events for school head" do
         get :index_school_head
         response.should redirect_to( pages_wrong_page_path )
         flash[:error].should =~ /вы не можете увидеть эту страницу/i
       end
     end
-    
+
     describe "for signed-in school-heads" do
       before(:each) do
         test_sign_in( @sh )
       end
-      
+
       it "should show events for school head" do
         get :index_school_head
         response.should be_success
@@ -57,7 +85,7 @@ describe EventsController do
       end
     end
   end
-  
+
   describe "GET 'index'" do
     describe "for non-signed users" do
       it "should deny access to show events" do
@@ -66,24 +94,24 @@ describe EventsController do
         flash[:notice].should =~ /войдите в систему как Классный руководитель/i
       end
     end
-    
+
     describe "for signed-in admins" do
       before(:each) do
         test_sign_in( @adm )
       end
-      
+
       it "should deny access to show events" do
         get :index
         response.should redirect_to( pages_wrong_page_path )
         flash[:error].should =~ /вы не можете увидеть эту страницу/i
       end
     end
-    
+
     describe "for signed-in class-heads" do
       before(:each) do
         test_sign_in( @ch )
       end
-      
+
       it "should show events" do
         get :index
         response.should be_success
@@ -91,7 +119,7 @@ describe EventsController do
       end
     end
   end
-  
+
   describe "GET 'new'" do
     describe "for non-signed users" do
       it "should deny access to show creating event's page" do
@@ -100,24 +128,24 @@ describe EventsController do
         flash[:notice].should =~ /войдите в систему как Классный руководитель/i
       end
     end
-    
+
     describe "for signed-in admins" do
       before(:each) do
         test_sign_in( @adm )
       end
-      
+
       it "should deny access to show creating event's page" do
         get :new
         response.should redirect_to( pages_wrong_page_path )
         flash[:error].should =~ /вы не можете увидеть эту страницу/i
       end
     end
-  
+
     describe "for signed-in class-heads" do
       before(:each) do
         test_sign_in( @ch )
       end
-      
+
       it "should show events" do
         get :new
         response.should be_success
@@ -140,36 +168,36 @@ describe EventsController do
         end.should_not change( Event, :count )
       end
     end
-    
+
     describe "for signin-in admins" do
       before(:each) do
         test_sign_in( @adm )
       end
-      
+
       it "should deny access" do
         post :create, :event => @event.attributes.merge( :event_cost => "333" )
         response.should redirect_to( pages_wrong_page_path )
         flash[:error].should =~ /вы не можете увидеть эту страницу/i
       end
-      
+
       it "should not create event" do
         expect do
           post :create, :event => @event.attributes.merge(:event_cost => " " )
         end.should_not change( Event, :count )
       end
     end
-    
+
     describe "for signed-in class-heads" do
       before(:each) do
         test_sign_in( @ch )
       end
-      
+
       it "should create event" do
         expect do
           post :create, :event => @event.attributes.merge( :event_cost => "333" )
         end.should change( Event, :count ).by( 1 )
       end
-      
+
       it "should not create event with wrong params" do
         expect do
           post :create, :event => @event.attributes.merge( :event_cost => " " )
@@ -186,24 +214,24 @@ describe EventsController do
         flash[:notice].should =~ /войдите в систему как Классный руководитель/i
       end
     end
-    
+
     describe "for signin-in admins" do
       before(:each) do
         test_sign_in( @adm )
       end
-      
+
       it "should deny access" do
         get :edit, :id => @event
         response.should redirect_to( pages_wrong_page_path )
         flash[:error].should =~ /вы не можете увидеть эту страницу/i
       end
     end
-    
+
     describe "for sign-in class-heads" do
       before(:each) do
         test_sign_in( @ch )
       end
-      
+
       it "should allow to edit events" do
         get :edit, :id => @event
         response.should be_success
@@ -211,50 +239,50 @@ describe EventsController do
       end
     end
   end
-  
+
   describe "PUT 'update" do
     describe "for non-signed users" do
       it "should deny access" do
-        put :update, :id => @event, 
+        put :update, :id => @event,
                      :order => @event.attributes.merge(:event_place => "fff" )
         response.should redirect_to( signin_path )
         flash[:notice].should =~ /войдите в систему как Классный руководитель/i
       end
     end
-    
+
     describe "for signed-in admins" do
       before(:each) do
         test_sign_in( @adm )
       end
-      
+
       it "should deny access" do
-        put :update, :id => @event, 
+        put :update, :id => @event,
                      :order => @event.attributes.merge(:event_place => "fff" )
         response.should redirect_to( pages_wrong_page_path )
         flash[:error].should =~ /вы не можете увидеть эту страницу/i
       end
     end
-    
+
     describe "for signed-in class-heads" do
       before(:each) do
         test_sign_in( @ch )
       end
-      
+
       it "should update event with correct params" do
         cost = 111
-        put :update, :id => @event, 
+        put :update, :id => @event,
                      :event => @event.attributes.merge( :event_cost => cost )
         @event.reload
         @event.event_cost.should == cost
       end
-      
+
       it "should reject to update event with wrong params" do
         text = "  "
-        put :update, :id => @event, 
+        put :update, :id => @event,
                      :order => @event.attributes.merge(:event_place => text )
         @event.reload
         @event.event_place.should_not == text
       end
-    end      
+    end
   end
 end
