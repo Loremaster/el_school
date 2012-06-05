4a4459edccf84f5e39a3c6ced83150bcdb48c131
L Serj <gelvior@gmail.com>
2012-06-05 17:40:40 +0400

    Tests added for pupil timetable page (controller).

--- spec/controllers/timetables_controller_spec.rb
+++ spec/controllers/timetables_controller_spec.rb
@@ -29,6 +29,47 @@ describe TimetablesController do
     @timetable = Timetable.create( @attr_timetable )
   end
 
+  describe "GET 'index_for_pupil'" do
+    before(:each) do
+      pupil = FactoryGirl.create( :pupil )
+      pupil.user.user_role = "pupil"
+      pupil.user.save!
+      @pupil = pupil.user
+    end
+
+    describe "for non-signed users" do
+      it "should deny access to show timetables" do
+        get :index_for_pupil
+        response.should redirect_to( signin_path )
+        flash[:notice].should =~ /войдите в систему как ученик/i
+      end
+    end
+
+    describe "for signed-in admins" do
+      before(:each) do
+        test_sign_in( @adm )
+      end
+
+      it "should deny access" do
+        get :index_for_pupil
+        response.should redirect_to( pages_wrong_page_path )
+        flash[:error].should =~ /вы не можете увидеть эту страницу/i
+      end
+    end
+
+    describe "for signed-in pupils" do
+      before(:each) do
+        test_sign_in( @pupil )
+      end
+
+      it "should show get timetables page" do
+        get :index_for_pupil
+        response.should be_success
+        flash[:error].should be_nil
+      end
+    end
+  end
+
   describe "GET 'index_for_parent'" do
     describe "for non-signed users" do
       it "should deny access to show timetables" do
