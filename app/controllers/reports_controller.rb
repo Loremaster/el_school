012552423d4e97a0c73d1ad8ef59f620d1dcf561
L Serj <gelvior@gmail.com>
2012-06-06 17:00:32 +0400

    Show events in reports.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,12 +3,26 @@ class ReportsController < ApplicationController
   before_filter :authenticate_class_heads, :only => [ :index ]
 
   def index
-    @school_class = get_class( current_user ); @meeting_exist = false
+    @school_class = get_class( current_user ); @meeting_exist = false; @event_exist = false
 
     unless @school_class.nil?
       @class_code = @school_class.class_code
+
       @meetings = Meeting.select{ |m| m.school_class.class_code == @class_code }            # Get parents meetings for class of current class head.
       @meeting_exist = @meetings.first ? true : false
+
+      @events = events_for( @class_code )
+      @event_exist = @events.first ? true : false
     end
   end
+
+
+  private
+
+    # Get events for school class via class code.
+    def events_for( class_code )
+      Event.select{|e| e.school_class.class_code == class_code }
+           .sort_by{ |e| e[:event_begin_date] }
+           .sort_by{ |e| e[:event_begin_time] }
+    end
 end
