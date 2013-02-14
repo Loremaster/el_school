3a218f38e4e484840ea2a7722568474ecc5a19ba
L Serj <gelvior@gmail.com>
2013-02-14 15:39:38 +0400

    Fix to show proper knowledge quality.

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -4,7 +4,8 @@ class Statistic
 
     unless marks.empty?
       nominals = marks.collect{ |m| m.nominal }
-      nominals.count{ |m| m == 5 or m == 4 }
+      positive_nominals = nominals.count{ |m| m == 5 or m == 4 }
+      positive_nominals / nominals.size.to_f                                                        # Convert on value to float to get flot value.
     else
       0                                                                                             # If there is no marks then orin 0.
     end
